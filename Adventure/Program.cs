3fa4f52c2fdc06c3c2d159ba9543658b6f3929a2
Tim Rayburn <tim@timrayburn.net>
2011-11-17 21:03:28 -0600

    added laugh command

--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -9,6 +9,38 @@ namespace Adventure
     {
         static void Main(string[] args)
         {
+            string currentLine;
+            List<ICommand> cmdList = new List<ICommand>();
+            cmdList.Add(new LaughCommand());
+
+            do 
+            {
+                Console.Write("> ");
+                currentLine = Console.ReadLine();
+
+                ICommand cmd = cmdList.FirstOrDefault(m => m.IsValid(currentLine));
+                if (cmd != null) cmd.Execute(currentLine);
+            } while (currentLine != "exit");
+        }
+    }
+
+    interface ICommand
+    {
+        bool IsValid(string cmd);
+        void Execute(string cmd);
+    }
+
+    class LaughCommand : ICommand
+    {
+
+        public bool IsValid(string cmd)
+        {
+            return cmd == "lol";
+        }
+
+        public void Execute(string cmd)
+        {
+            Console.WriteLine("You laugh out loud!");
         }
     }
 }
